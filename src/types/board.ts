7443f86912dba58b5adb59d8c9eceb944d4cd880
ChrismanE21 <54912180+ChrismanE21@users.noreply.github.com>
2023-02-12 18:06:43 -0600

    board now creates blank board of pieces

--- src/types/board.ts
+++ src/types/board.ts
@@ -5,24 +5,25 @@ export class board{
     this.sizeX = sizeX;
     this.sizeY = sizeY;
 
+    this.gamePieces = [];
+
     // loop to generate space for pieces
     for(let i: number = 0; i < this.sizeX; i++){// x axis
-      // this.gamePieces.push([piece])
+      const tempArray: Piece[] = [];
       for(let j: number = 0; j < this.sizeY; j++){// y axis
-        // this.gamePieces[i].push(piece)
+        const placeHolderPiece = new Piece(); // params to add : 'emptySpace', 'null'
+        tempArray.push(placeHolderPiece);
       }
+      this.gamePieces.push(tempArray);
     }
+    console.log(this.gamePieces.length);
+    console.log(this.gamePieces[0].length);
     this.createBoard();
   }
 
   // makes empty board with pieces in right place. Changes turn to white. Calls printBoard()
   createBoard(){
-    for(let i: number = 0; i < this.sizeX; i++){// x axis
-      // this.gamePieces.push([piece])
-      for(let j: number = 0; j < this.sizeY; j++){// y axis
-        // this.gamePieces[i].push(piece)
-      }
-    }
+
   }
 
   // cin move vaildation loop that returns 1 if move maded is vaild, returns 0 if players wish to quit. Calls makeMove()
@@ -47,7 +48,7 @@ export class board{
 
   private sizeX: number;
   private sizeY: number;
-  //private gamePieces: Piece[][];
+  private gamePieces: Piece[][];
   //private playerMove: boolean = false; // fasle = white's turn // true = black's trun
 
 
