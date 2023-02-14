d3c9817ef19be08fa08a49cdc45158ba5fbf329a
ChrismanE21 <54912180+ChrismanE21@users.noreply.github.com>
2023-02-14 15:30:07 -0600

    fix print. now we can print board

--- src/types/board.ts
+++ src/types/board.ts
@@ -4,7 +4,7 @@ export class Board {
   constructor(sizeX: number = 8, sizeY: number = 8) {
     this.sizeX = sizeX;
     this.sizeY = sizeY;
-    
+
     this.gamePieces = [];
 
     // loop to generate space for pieces
@@ -13,7 +13,7 @@ export class Board {
       const tempArray: Piece[] = [];
       for (let j: number = 0; j < this.sizeX; j += 1) {
         // x axis
-        let tempPiece = new Piece("null", "null");
+        const tempPiece = new Piece('null', 'null');
         tempArray.push(tempPiece);
       }
       this.gamePieces.push(tempArray);
@@ -31,39 +31,41 @@ export class Board {
 
   // cin move vaildation loop that returns 1 if move maded is vaild, returns 0 if players wish to quit. Calls makeMove()
   checkMove(): void {
+    this.sizeX = this.sizeX + 1 - 1;
     console.log('checkMove not Impetmented yet');
   }
 
   // once move is identified to be okey. change the board to refelct that move. Calls printBoard()
   makeMove(): void {
+    this.sizeX = this.sizeX + 1 - 1;
     console.log('makeMove not Impetmented yet');
   }
 
   // prints board to the screen / may have to be private
   printBoard(): void {
-    console.log(`printBoard was called. Board is ${this.sizeX} by ${this.sizeY}`); /*
+    console.log(`printBoard was called. Board is ${this.sizeX} by ${this.sizeY}`);
     for (let i: number = 0; i < this.sizeY; i += 1) {
-      const tempString: string = '';
-      for (let j: number = 0; j < this.sizeX; i += 1) {
-        // tempString = tempString.concat(' ', i.toString(), ' ', j.toString());
+      let tempString: string = '';
+      for (let j: number = 0; j < this.sizeX; j += 1) {
+        tempString = tempString.concat(' [', i.toString(), ', ', j.toString(), '] ');
       }
       console.log(tempString);
-    } */
+    }
   }
 
-   // left and right board size
+  // left and right board size
   private sizeX: number;
 
   // up and down board size
-  private sizeY: number; 
+  private sizeY: number;
 
-   // 0,0 is the top left corner of the board / [x][y]
-  private gamePieces: Piece[][]; 
-  
+  // 0,0 is the top left corner of the board / [x][y]
+  private gamePieces: Piece[][];
+
   // this is our empty square / null was the og idea but it was hard to implement
   // The space lost isn't that bad from this structure for we are dealing with board of 8 by 8 for this project
   // private nullPiece: Piece; Not sure if this is the best way
-  
+
   // false = lowerCase's turn // true = upperCase's turn
   private playerMove: boolean = false;
 }
