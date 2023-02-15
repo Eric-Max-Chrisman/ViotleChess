f3fa435037c1f9186f8b5e9ebe5e578b8ff3c1ce
Eric-Max-Chrisman <122303933+Eric-Max-Chrisman@users.noreply.github.com>
2023-02-15 12:30:19 -0600

    Added make move and create null

--- src/types/board.ts
+++ src/types/board.ts
@@ -13,7 +13,7 @@ export class Board {
       const tempArray: Piece[] = [];
       for (let j: number = 0; j < this.sizeX; j += 1) {
         // x axis
-        const tempPiece = new Piece('null', 0, new Point2D(j, i));
+        const tempPiece = new Piece('null', 3, new Point2D(j, i));
         tempArray.push(tempPiece);
       }
       this.gamePieces.push(tempArray);
@@ -21,9 +21,18 @@ export class Board {
   }
   
   // once move is identified to be okey. change the board to refelct that move. Calls printBoard()
-  private makeMove(): void {
-    this.sizeX = this.sizeX + 1 - 1;
-    console.log('makeMove not Impetmented yet');
+  makeMove(x: number, y: number, newX: number, newY: number): void {
+    this.gamePieces[newX][newY] = this.gamePieces[x][y];
+    this.setNullPiece(x, y);
+    // copy (x,y)'s piece into the (newX, newY)
+    // then set (x,y) to null piece
+    // if king taken, call it quits and Win state.
+  }
+  
+  setNullPiece(x:number, y:number){
+    const piecePtr = this.gamePieces[x][y];
+    piecePtr.setName('null');
+    piecePtr.setColor(3);
   }
 
   // prints board to the screen / may have to be private
