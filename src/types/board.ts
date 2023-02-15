df1744e04b6bae32e616f3c446b2eb181a0bee4a
Eric-Max-Chrisman <122303933+Eric-Max-Chrisman@users.noreply.github.com>
2023-02-15 08:56:31 -0600

    Update board.ts
    deleted checkMove. It is the responability of the templates to check if move is vaild.

--- src/types/board.ts
+++ src/types/board.ts
@@ -19,23 +19,7 @@ export class Board {
       this.gamePieces.push(tempArray);
     }
   }
-
-  // this function checks if a given move is valid. returns 1 if move maded is vaild, returns 1 if move is invaild,
-  // returns 2 if players wish to quit. Calls makeMove() if move is vaild
-  checkMove(): number {
-    let tempBool: number = 0;
-    tempBool = 1;
-
-    // step one, check if move is vaild (Hayden)
-
-    if (tempBool === 1) {
-      this.makeMove();
-    }
-
-    console.log(`CHECK MOVE: returning ${tempBool}`);
-    return tempBool;
-  }
-
+  
   // once move is identified to be okey. change the board to refelct that move. Calls printBoard()
   private makeMove(): void {
     this.sizeX = this.sizeX + 1 - 1;
