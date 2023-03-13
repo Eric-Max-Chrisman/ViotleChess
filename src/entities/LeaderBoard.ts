a69429bbe1cef117202590395fe2e23abc6f37d0
Eric-Max-Chrisman <122303933+Eric-Max-Chrisman@users.noreply.github.com>
2023-03-13 08:50:08 -0500

    Create LeaderBoard

---
+++ src/entities/LeaderBoard.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, Relation } from 'typeorm';
+import { User } from './User';
+import { Set } from './Set';
+
+@Entity()
+export class LeaderBoard {
+  @Column({ unique: false })
+  wins: number;
+  
+  //ToDo
+  @ManyToOne set
+  @OneToOne user
+}
