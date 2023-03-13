6afe319b4fb6ffaf171c1dc6f07c3ec3afccabef
Hayden-Althoff <hayden.althoff@smail.astate.edu>
2023-03-13 12:43:47 -0500

    Entity set up for Point2D

--- src/entities/Point2D.ts
+++ src/entities/Point2D.ts
@@ -1,1 +1,34 @@
 // ToDo
+import { Entity, Column, ManyToMany, Relation, JoinTable } from 'typeorm';
+
+@Entity()
+class Point2D {
+  @Column()
+  private x: number;
+
+  @Column()
+  private y: number;
+
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  getX(): number {
+    return this.x;
+  }
+
+  getY(): number {
+    return this.y;
+  }
+
+  setX(newX: number): void {
+    this.x = newX;
+  }
+
+  setY(newY: number): void {
+    this.y = newY;
+  }
+}
+
+export { Point2D };
